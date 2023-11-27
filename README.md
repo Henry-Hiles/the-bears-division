6fd1084e6dab32e5d91a2c0f53616c529ea49119
Henry Hiles <henry@henryhiles.com>
2023-11-27 17:31:15 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Website Astro Rewrite
+A rewrite of https://henryhiles.com using Astro instead of ASP.Net Core Razor Pages.
---
+++ src/categories.js
@@ -0,0 +1,29 @@
+export default Object.entries({
+	all: {
+		title: "All",
+		default: true,
+		descriptionTitle: "Welcome to the blog!",
+		description:
+			"Hello, and welcome to my blog. I post about Discord Bot Development, Web Development, Linux, and other tech-related subjects. You can filter and search in the sidebar, and click on an article to read more."
+	},
+	linux: {
+		title: "Linux",
+		description:
+			"I use Linux on my PC daily, ever since I switched from Windows many years ago. I use Arch Linux currently, but I sometimes distrohop. You can find my dotfiles [here](https://github.com/Henry-Hiles/dotfiles)."
+	},
+	web: {
+		title: "Web Development",
+		description:
+			"There are over 2 billion websites, and the number is only increasing. I post articles here about CSS and HTML tips, making static sites or webapps with Astro / Next.JS, Web Development news, and more. [Order a custom website from me here](/#hire)."
+	},
+	bot: {
+		title: "Discord Bot Development",
+		description:
+			"Discord is a chat app which has over 300 million registered accounts. There are Discord Bots which can automate tasks or add new features to improve your experience. I post articles about Discord Bots here. I can also [make a discord bot for you](/discord-bot-developer)."
+	},
+	other: {
+		title: "Other",
+		description:
+			"This section of my blog is for all my articles that don't fit into any of the other catagories."
+	}
+})
